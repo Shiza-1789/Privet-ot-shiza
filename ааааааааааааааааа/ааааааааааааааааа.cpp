50045a21884b8f1ca5f29696b7d3c3dd5e3c04dd
Shiza-1789 <77673753+Shiza-1789@users.noreply.github.com>
2021-01-23 14:12:48 +0300

    Privet-ot-shiza

---
+++ ааааааааааааааааа/ааааааааааааааааа.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	cout << "\n Выполнили Авджян Вероника и Сарычев Максим 219/1 " << "\n" << "\n";
+	int s = 0;
+	for (int n = 0; n < 150; n++)
+	{
+		if ((n % 5) == 0)
+			s = s + n;
+	}
+	cout << " Сумма = " << s << "\n" << "\n";
+	system("pause");
+}
